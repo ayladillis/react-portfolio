258c5fbe62cfefb5aaa79f97bf8e8aa08b0a6922
Ayla Dillis <aayladillis@gmail.com>
2020-08-18 12:18:49 -0700

    adding blog cards

--- client/src/components/Blog/index.js
+++ client/src/components/Blog/index.js
@@ -5,9 +5,37 @@ function Blog() {
     <div className="jumbotron jumbotron-fluid">
         <div className="container">
             <h1 className="display-4">Blog</h1>
-            <p className="lead">
-      
-            </p>
+            <br></br>
+            <div className="card-group">
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">An Amateur Explains ‘.querySelector’</h5>
+      <p className="card-text">My favorite thing about code is that any question you could ever have can be found with Google search, hardly any other profession...</p>
+      <p className="card-text"><small className="text-muted">July, 5th 2020</small></p>
+    </div>
+  </div>
+  <br />
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">Battle of the Browsers</h5>
+      <p className="card-text">Today ‘searching it online’ and ‘Googling it’ are virtually the same thing, Google is the most used and well-known web browser...</p>
+      <p className="card-text"><small className="text-muted">August, 3rd 2020</small></p>
+    </div>
+  </div>
+  <br />
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
         </div>
     </div>
     );
