5183a44d32345482b2cb2e458ef5e71bb5314ecf
Ayla Dillis <aayladillis@gmail.com>
2020-08-18 10:04:11 -0700

    adding blog jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Navbar from "./components/Navbar/index";
 import Jumbotron from "./components/Jumbotron/index";
-import PortfolioNav from "./components/PortfolioNav/index";
+// import PortfolioNav from "./components/PortfolioNav/index";
 import AboutMe from "./components/AboutMe/index";
+import Blog from "./components/Blog/index";
 import Footer from "./components/Footer/index";
 // import Chart from "./components/Chart/index";
 
@@ -13,8 +14,8 @@ function App() {
       <Jumbotron />
 
       {/* <Chart /> */}
-
       <AboutMe />
+      <Blog />
       <Footer />
     </div>
   );
